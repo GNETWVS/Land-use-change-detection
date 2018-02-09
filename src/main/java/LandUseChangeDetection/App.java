a505d1025207e2dd43063408530f8b0f94ca48e7
Arthur Lukin <lukin.arthur@yandex.ru>
2018-02-09 19:44:09 +0300

    Initial commit

---
+++ src/main/java/LandUseChangeDetection/App.java
@@ -0,0 +1,13 @@
+package LandUseChangeDetection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
