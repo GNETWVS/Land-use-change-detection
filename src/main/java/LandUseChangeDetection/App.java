0fad01c84e87a7afc71728bb29420d7b58de4d78
Arthur Lukin <lukin.arthur@yandex.ru>
2018-02-16 19:28:50 +0300

    Added: JavaFX Form

--- src/main/java/LandUseChangeDetection/App.java
+++ src/main/java/LandUseChangeDetection/App.java
@@ -1,13 +1,31 @@
 package LandUseChangeDetection;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+
+        primaryStage.setTitle("Land-Use Change Detector");
+        primaryStage.setScene(new Scene(root, 800, 600));
+
+        primaryStage.show();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
     }
 }
