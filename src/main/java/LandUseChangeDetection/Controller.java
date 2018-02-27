69bc5bc107db38081af415822ee6561bf0e223f6
Artur Lukin <lukin.arthur@yandex.ru>
2018-02-28 01:22:37 +0300

    Fixed: Add Sentinel 2 Toolbox Dependency

--- src/main/java/LandUseChangeDetection/Controller.java
+++ src/main/java/LandUseChangeDetection/Controller.java
@@ -1,11 +1,30 @@
 package LandUseChangeDetection;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Controller {
+
+    public void openLearnOSMForm(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/learnOSMForm.fxml"));
+        try {
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Learn SVM by OSM");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void openNextGisLearningForm(ActionEvent actionEvent) {
         StackPane layout = new StackPane();
